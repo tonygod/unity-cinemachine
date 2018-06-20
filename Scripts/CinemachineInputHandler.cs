5effbb0454d6ed5c51d12f5877a6200d189d4f33
Tony Godfrey <tonygod@sharkappsllc.com>
2018-06-20 15:33:18 -0700

    Initial commit of sources

---
+++ Scripts/CinemachineInputHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class CinemachineInputHandler : MonoBehaviour
+{
+    private CinemachineCore.AxisInputDelegate DefaultAxisInput;
+
+
+    private void Awake()
+    {
+        DefaultAxisInput = CinemachineCore.GetInputAxis;
+    }
+
+
+    float DisableAxisInputDelegate(string axisName)
+    {
+        return 0f;
+    }
+
+
+    public void DisableInput()
+    {
+        CinemachineCore.GetInputAxis = DisableAxisInputDelegate;
+    }
+
+
+    public void EnableInput()
+    {
+        CinemachineCore.GetInputAxis = DefaultAxisInput;
+    }
+}
